aa6c283f0bdbee6ac7835de6ba47ca59b04ae6d0
John Carlo Roberto <10111643+Irizwaririz@users.noreply.github.com>
2023-05-23 08:35:30 +0800

    Add ml branch

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,15 @@
-from flask import Flask
-
-from flaskr import blog
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template("base.html")
     
     @app.route('/hello')
     def hello_world():
         return "Hello world!"
-
-    app.register_blueprint(blog.bp)
 
     return app
